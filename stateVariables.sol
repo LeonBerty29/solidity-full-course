5c186e83f3663f19513368b34b660c294abe14cf
LeonBerty29 <leonbertrand29.dev@gmail.com>
2025-04-23 10:01:54 +0100

    learned about state variables, local variables and global variables

---
+++ stateVariables.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract StateVariables {
+    string public myState;
+    uint256 public myNum;
+
+    string public defaultText = "Hey Default Text";
+    uint256 public defaultNum = 5;
+
+    bytes public defaultBytes = "Hey default Bytes";
+
+    uint256[] public myNumber;
+
+    constructor(string memory _text, uint _no) {
+        myState = _text;
+        myNum = _no;
+    }
+
+    function update(string memory _text, uint _no) public {
+        myState = _text;
+        myNum = _no;
+    }
+    
+}
