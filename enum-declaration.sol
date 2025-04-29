009d2b9c9e868c58367ddd4e77bf7f2d8a9da7b5
LeonBerty29 <leonbertrand29.dev@gmail.com>
2025-04-29 14:58:03 +0100

    importing enums from a second file

---
+++ enum-declaration.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+enum ShippingStatus {
+    Pending,
+    Shipped,
+    Accepted,
+    Rejected,
+    Canceled
+}
---
+++ import-enum.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+import "./enum-declaration.sol";
+
+contract Enum {
+    // Enum representing shipping status
+
+    // Whenever you define Enums, the default value is first element listed
+    // In this case the default value is Pending
+
+        // Pending - 0
+        // Shipped - 1
+        // Accepted - 2
+        // Rejected - 3
+        // Canceled - 4
+
+
+
+    ShippingStatus public status;
+
+    function get() public view returns(ShippingStatus) {
+        return status;
+    }
+
+    // Update status by passing uint into input
+
+    function set(ShippingStatus _status) public {
+        status = _status;
+    }
+
+    // You can update to a specific enum like this
+    function cancel() public {
+        status = ShippingStatus.Canceled;
+    }
+
+    // delete resets the enum to it's first value, 0
+
+    function reset() public {
+        delete status;
+    }
+}
