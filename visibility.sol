d37038e8fc906c8c5752fb5544e3141a49e2f35d
LeonBerty29 <leonbertrand29.dev@gmail.com>
2025-05-14 15:40:44 +0100

    leaened about visibility

---
+++ visibility.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Base {
+    // Private function can only ve called
+    // contracts that inherits this contract cannot call the private function
+    
+    function privateFunc() private pure returns (string  memory) {
+        return "private function called";
+    }
+
+    function testPrivateFunc() public pure returns (string memory) {
+        return privateFunc();
+    }
+
+    // Internal function can be called
+    // - inside this contract
+    // - inside contracts that inherit this contract
+
+    function internalFunc() internal pure returns (string memory) {
+        return "internal function called";
+    }
+
+    function testInternalFunc() public pure virtual returns (string memory) {
+        return internalFunc();
+    }
+
+
+    // Public functions can be called
+    // - inside this contract
+    // - inside contracts that inherit this contract
+    // - by other contracts and accounts
+    function publicFunc() public pure returns (string memory) {
+        return "public funtion called";
+    }
+
+    // External functions can only be called 
+    // - by other contracts and accounts
+    function externalFunc() external  pure returns (string memory) {
+        return "external function called";
+    }
+
+    // This function will not compile since we're trying to call 
+    // an external function here.
+    // function testExternalFunc() public pure returns (string memory) {
+        // return externalFunc();
+    // }
+
+    // State variables
+    string private privateVar = "my private variable";
+    string internal internalVar = "my internal variable";
+    string public publicVar = "my public variable";
+    // State variables cannot be external so this code won't compile
+    // string external externalVar = "my external variable";
+}
+
+
+contract Child is Base {
+    // Inherited contracts do not have access to private functions
+    // and state variables.
+    function testInternalFunc() public pure override returns (string memory) {
+        return internalFunc();
+    }
+}
